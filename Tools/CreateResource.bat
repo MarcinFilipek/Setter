588c0600f29d0d059ba0be6382d7359503a0bac0
Marcin <you@example.com>
2021-03-13 12:13:14 +0100

    added Tools with resources

---
+++ Tools/CreateResource.bat
@@ -0,0 +1,2 @@
+"../Tools/Napisy i grafika 1/MonoImagesConverter.exe" -p "resource/Graphics/graphics.png" -d "resource/Graphics/graphicsId.txt" -h "resource/Graphics/graphicsIdEnum.h.tml" -s "resource/Graphics/flashGraphics.h.tml" -o "resourceOutput" -l
+"../Tools/MonoImageTextConverter/MonoImageTextConverter.exe" "resource" "resourceOutput" "resourceOutput"
---
+++ Tools/CreateResource.sh
@@ -0,0 +1,4 @@
+chmod +x "../Tools/MonoImageTextConverter/MonoImageTextConverter.exe"
+chmod +x "../Tools/Napisy i grafika 1/MonoImagesConverter.exe"
+"../Tools/Napisy i grafika 1/MonoImagesConverter.exe" -p "resource/Graphics/graphics.png" -d "resource/Graphics/graphicsId.txt" -h "resource/Graphics/graphicsIdEnum.h.tml" -s "resource/Graphics/flashGraphics.h.tml" -o "resourceOutput" -l
+"../Tools/MonoImageTextConverter/MonoImageTextConverter.exe" "resource" "resourceOutput" "resourceOutput"
